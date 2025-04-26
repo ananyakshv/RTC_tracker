35aaf61992ad8efc0e90f6f1014f37192bc15674
Ananya Keshav <ananyak752004@gmail.com>
2025-04-26 11:13:50 +0530

    Deleted extra llm_demo directory

--- llm_demo/check_models.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCvYyWePmmWflfDiqopdSW0_-vbpiTU23w")
-
-for m in genai.list_models():
-    print(m.name)
--- llm_demo/mini_llm/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import extract_names_view
-
-urlpatterns = [
-    path("extract-names/", extract_names_view, name="extract_names"),
-]
